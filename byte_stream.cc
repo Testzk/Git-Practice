126e91f5f540b5f3aee2b189db7c3827f4df271f
Testzk <83399915+Testzk@users.noreply.github.com>
2021-04-29 23:27:36 +0800

    Update byte_stream.cc

--- byte_stream.cc
+++ byte_stream.cc
@@ -25,6 +25,33 @@ size_t ByteStream::write(const string &data) {
     _accumuWritten+=writedDataNum;
     return writedDataNum;
 }
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some codeTestzk Add some code
+
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
+Testzk Add some code
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
